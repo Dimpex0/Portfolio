a7ea2b8fc72b2559040b323047563eaf903c2f3b
Dimpex <mitkodd2005@gmail.com>
2023-08-23 18:38:05 +0300

    made website responsive

--- index.js
+++ index.js
@@ -26,3 +26,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+const barsIcon = document.getElementsByClassName('fa-bars')[0];
+const nav = document.querySelector('.nav');
+const closeBtn = document.querySelector('.closebtn');
+
+function expandNav() {
+  barsIcon.style.display = 'none';
+  nav.classList.remove(...nav.classList);
+  nav.classList.add('nav');
+  nav.classList.add('open');
+  nav.style.display = 'flex';
+  closeBtn.style.display = 'block';
+}
+
+function closeNav() {
+  closeBtn.style.display = 'none';
+  barsIcon.style.display = 'block';
+  nav.style.display = 'none';
+}
